0927e7fa3108cd31971774ad0c1b782380c28776
Hookem22 <willparks@Will-Parkss-Mac-mini.local>
2015-05-19 13:38:35 -0500

    Initial Commit

---
+++ Mayfly/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Mayfly
+//
+//  Created by Will Parks on 5/19/15.
+//  Copyright (c) 2015 Mayfly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Mayfly/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Mayfly
+//
+//  Created by Will Parks on 5/19/15.
+//  Copyright (c) 2015 Mayfly. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
